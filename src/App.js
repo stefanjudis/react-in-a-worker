f989ef6204e57357087ca10f30897fb2590279ab
stefan judis <stefanjudis@gmail.com>
2019-05-21 15:36:25 +0200

    clean up routes

--- src/App.js
+++ src/App.js
@@ -4,33 +4,25 @@ import { isSW } from './util/is-sw';
 import Page from './pages/Page';
 import routes from './routes';
 
+const getRoutes = () =>
+  routes.map((route, index) => (
+    <Route
+      key={index}
+      path={route.path}
+      exact={route.exact}
+      render={props => {
+        return <Page {...props} component={route.component} />;
+      }}
+    />
+  ));
+
 function App(props) {
   return isSW() ? (
     <StaticRouter location={props.match.path} context={props.data}>
-      {routes.map((route, index) => (
-        <Route
-          key={index}
-          path={route.path}
-          exact={route.exact}
-          render={props => {
-            return <Page {...props} component={route.component} />;
-          }}
-        />
-      ))}
+      {getRoutes()}
     </StaticRouter>
   ) : (
-    <BrowserRouter>
-      {routes.map((route, index) => (
-        <Route
-          key={index}
-          path={route.path}
-          exact={route.exact}
-          render={props => {
-            return <Page {...props} component={route.component} />;
-          }}
-        />
-      ))}
-    </BrowserRouter>
+    <BrowserRouter>{getRoutes()}</BrowserRouter>
   );
 }
 
